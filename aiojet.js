4a2f6bc2ec2ce6d6bafc2ab745e06b85a157daf7
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-07-28 21:22:17 +0530

    Update aiojet.js

--- aiojet.js
+++ aiojet.js
@@ -1,3 +1,61 @@
+function externalLinks() {
+ for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
+  var b = c[a];
+  b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank", b.rel = "nofollow")
+ }
+}
+externalLinks();
+window.addEventListener('DOMContentLoaded', function() {
+  try {
+    // Get all the image elements in the blog post content
+    var images = document.querySelectorAll('img');
+
+    // Get the post title
+    var postTitleElement = document.querySelector('.entry-title');
+    
+    if (!postTitleElement) {
+      console.error('Post title element not found');
+      return;
+    }
+
+    var postTitle = postTitleElement.textContent;
+
+    // Loop through each image and set the alt text if it's not already set
+    for (var i = 0; i < images.length; i++) {
+      if (!images[i].hasAttribute('alt') || images[i].getAttribute('alt').trim() === '') {
+        images[i].setAttribute('alt', postTitle);
+      }
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+});
+
+var codeElements = document.querySelectorAll("pre");
+    codeElements.forEach(function(element) {
+        element.addEventListener("dblclick", function() {
+            copyToClipboard(element);
+        });
+    });
+    function copyToClipboard(element) {
+        var tempTextArea = document.createElement("textarea");
+        tempTextArea.value = element.textContent;
+        document.body.appendChild(tempTextArea);
+        tempTextArea.select();
+        tempTextArea.setSelectionRange(0, 99999); 
+        document.execCommand("copy");
+        document.body.removeChild(tempTextArea);
+        
+        showBottomAlert();
+    }
+    function showBottomAlert() {
+        var bottomAlert = document.getElementById("bottomAlert");
+        bottomAlert.style.display = "block";
+        setTimeout(function() {
+            bottomAlert.style.display = "none";
+        }, 3000);
+    }
+
 var allowedURLs = [
   "https://dkhek.blogspot.com",
   "https://fletrothemes.blogspot.com/"
