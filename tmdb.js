185ec7f67b017f2449fb9589cfc7f259d88ff5bc
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-07-04 05:44:22 +0530

    Update tmdb.js

--- tmdb.js
+++ tmdb.js
@@ -214,7 +214,6 @@ window.liveSearch = liveSearch;
 window.performSearch = performSearch;
 
 // Constants
-const TMDB_API_KEY = "7bffed716d50c95ed1c4790cfab4866a";
 let currentPage = 1;
 let currentSearchPage = 1;
 let currentBrowsePage = 1;
